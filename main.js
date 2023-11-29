cbf917bdf05becd354c8c1f380e0214a7a0c1be4
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-29 02:57:02 +0100

    feat: To do list completo

--- main.js
+++ main.js
@@ -3,11 +3,24 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-
-      
+      todos: [
+        { text: "Fare passeggiata", done: true },
+        { text: "Leggere libro", done: false },
+        { text: "Fare Sport", done: true },
+        { text: "Meditare", done: false },
+      ],
+      newTodo: ''
     };
   },
   methods: {
-   
-}
+    removeTodo: function(index) {
+      this.todos.splice(index, 1);
+    },
+    addTodo: function() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push({ text: this.newTodo, done: false });
+        this.newTodo = '';
+      }
+    }
+  }
 }).mount("#app");
